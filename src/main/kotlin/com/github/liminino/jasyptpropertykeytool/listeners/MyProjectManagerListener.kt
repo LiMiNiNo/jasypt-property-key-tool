fd4c3c8cad19879050a1734bb5af2e6832513bf7
GitHub Action <action@github.com>
2021-04-26 13:04:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/liminino/jasyptpropertykeytool/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/liminino/jasyptpropertykeytool/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.liminino.jasyptpropertykeytool.listeners
 
+import com.github.liminino.jasyptpropertykeytool.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/liminino/jasyptpropertykeytool/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.liminino.jasyptpropertykeytool.services
+
+import com.github.liminino.jasyptpropertykeytool.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
